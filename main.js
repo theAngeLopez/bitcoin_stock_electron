06a2c6eb6e66fcb8a8de6b0309860a4bb86a6251
AngeLopez <angel@angelopez.ca>
2016-08-15 22:43:04 -0400

    Added request

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 200, height: 400, frame:false});
+  win = new BrowserWindow({width: 400, height: 200, frame:false});
 
   // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/index.html`)
